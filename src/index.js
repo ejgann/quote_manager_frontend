aaf1a0d4fddc162d8bf6ed89ead76e3901c8c558
Emily Gann <aspentree79@gmail.com>
2020-12-09 12:30:15 -0500

    wrote complete get fetch request that is working to successfully display api data

--- src/index.js
+++ src/index.js
@@ -8,5 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
 function getQuotes() {
     fetch(endPoint)
     .then(res => res.json())
-    .then(json => console.log(json));
+    .then(quote => {
+        quote.data.forEach(quote => {
+            const quoteMarkup = `
+                <tr data-id=${quote.id}>
+                    <th scope="row">1</th>
+                    <td>${quote.attributes.company} </td>
+                    <td>${quote.attributes.website} </td>
+                    <td>${quote.attributes.in_budget} </td>
+                    <td>${quote.attributes.quote_amount} </td>
+                  </tr>`;
+
+            document.querySelector('#quote_container').innerHTML += quoteMarkup
+        })
+    })
 }
