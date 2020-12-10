089201bf296733186b4b0258e14d7938011f054b
Emily Gann <aspentree79@gmail.com>
2020-12-10 16:15:21 -0500

    making some style changes to form and adding labels for input fields

--- src/index.js
+++ src/index.js
@@ -28,6 +28,29 @@ function getQuotes() {
     })
 }
 
+function getRadioVal(form, name) {
+    var val;
+    // get list of radio buttons with specified name
+    var radios = form.elements[name];
+
+    // loop through list of radio buttons
+    for (var i=0, len = radios.length ; i<len; i++) {
+        if (radios[i].checked ) {  
+            // radio checked?
+            val = radios[i].value;
+            // if so, hold its value in val
+            break;
+            // and break out of the for loop
+        }
+    }
+    return val;
+    // return value of checked radio or undefined in none are checked
+}
+
+// get value of selected 'budget' radio button in 'budgetRadio'
+var val = getRadioVal( document.getElementById)('quoteForm', 'budget' );
+console.log(val);
+
 function createFormHander(e) {
     e.preventDefault()
     // define const vars that identify form inputs and links them to document query selectors....will be referenced in last line of this function as postQuote with params using the same const vars.
@@ -35,14 +58,14 @@ function createFormHander(e) {
     const companyInput = document.querySelector('#input-company') 
     "input-company".value
     const urlInput = document.querySelector('#input-url').value
-    const inBudget = document.querySelector
 
-    
+    // const inBudget = document.querySelector('#budgetRadio').value
+
     const quoteAmountInput = document.querySelector('#input-quote-amount').value
     const projectInput = document.querySelector('#projects').value
     const projectId = parseInt(projectInput)
     
-    postQuote(companyInput, urlInput, quoteAmountInput, projectInput) 
+    postQuote(companyInput, urlInput, inBudget, quoteAmountInput, projectInput) 
 }
 
 function postQuote(company, website, in_budget, quote_amount, project_id) {
