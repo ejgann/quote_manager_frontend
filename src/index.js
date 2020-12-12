5c23cf84c76ce49a60089a1beda4fc20df621f54
Emily Gann <aspentree79@gmail.com>
2020-12-11 20:09:02 -0500

    adding some style and fonts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('LOADED');
     getQuotes();
 
-    let quoteForm = document.querySelector('#quoteForm')
+    const quoteForm = document.querySelector('#quoteForm')
 
     quoteForm.addEventListener('submit', (e) => createFormHandler(e))
 });
@@ -15,13 +15,14 @@ function getQuotes() {
     .then(quote => {
         quote.data.forEach(quote => {
             const quoteMarkup = `
-                <tr data-id=${quote.id}>
-                    <th scope="row"></th>
-                    <td>${quote.attributes.company} </td>
-                    <td>${quote.attributes.website} </td>
-                    <td>${quote.attributes.in_budget} </td>
-                    <td>${quote.attributes.quote_amount} </td>
-                  </tr>`;
+                <div data-id=${quote.id}>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>${quote.attributes.company} </td>
+                        <td>${quote.attributes.website} </td>
+                        <td>${quote.attributes.quote_amount} </td>
+                    </tr>
+                </div>`;
 
             document.querySelector('#quote_container').innerHTML += quoteMarkup
         })
@@ -30,23 +31,41 @@ function getQuotes() {
 
 
 
-function createFormHander(e) {
+function createFormHandler(e) {
     e.preventDefault()
     // define const vars that identify form inputs and links them to document query selectors....will be referenced in last line of this function as postQuote with params using the same const vars.
 
-    const companyInput = document.querySelector('#input-company') 
-    "input-company".value
+    const companyInput = document.querySelector('#input-company').value
     const urlInput = document.querySelector('#input-url').value
-
-    // const inBudget = document.querySelector('#budgetRadio').value
-
     const quoteAmountInput = document.querySelector('#input-quote-amount').value
     const projectInput = document.querySelector('#projects').value
     const projectId = parseInt(projectInput)
-    
     postQuote(companyInput, urlInput, quoteAmountInput, projectInput) 
 }
 
-function postQuote(company, website, in_budget, quote_amount, project_id) {
-    console.log(company, website, quote_amount, project_id);
+
+function postQuote(company, website, quote_amount, project_id) {
+    // console.log(company, website, quote_amount, project_id);
+    let bodyData = {company, website, quote_amount, project_id}
+
+    fetch(endPoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(quote => {
+        // console.log(quote);
+        
+        const quoteMarkup = `
+                <div data-id=${quote.id}>
+                    <tr>
+                        <th scope="row"></th>
+                        <td>${quote.attributes.company} </td>
+                        <td>${quote.attributes.website} </td>
+                        <td>${quote.attributes.quote_amount} </td>
+                    </tr>
+                </div>`; 
+                document.querySelector('#quote-container').innerHTML += quoteMarkup;
+    })
 }
