5f605d9d8e940917ffcc4e883022a4233760b74e
Emily Gann <aspentree79@gmail.com>
2020-12-28 15:08:52 -0500

    accordian code working

--- src/index.js
+++ src/index.js
@@ -51,33 +51,33 @@ function postQuote(company, website, quote_amount, project_id) {
 }
 
 
-// PROJECT MODAL
+// // PROJECT MODAL
 
-// Get the modal
-const modal = document.getElementById("myModal");
+// // Get the modal
+// const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("myButton");
+// // Get the button that opens the modal
+// const btn = document.getElementById("myButton");
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.addEventListener('click', () => {
-    modal.style.display = "block";
-})
+// // When the user clicks on the button, open the modal
+// btn.addEventListener('click', () => {
+//     modal.style.display = "block";
+// })
 
-// When the user clicks on <span> (x), close the modal
-span.addEventListener('click', function () {
-    modal.style.display = "none";
-})
+// // When the user clicks on <span> (x), close the modal
+// span.addEventListener('click', function () {
+//     modal.style.display = "none";
+// })
 
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", function(e) {
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
-})
+// // When the user clicks anywhere outside of the modal, close it
+// window.addEventListener("click", function(e) {
+//     if (e.target == modal) {
+//         modal.style.display = "none";
+//     }
+// })
 
 
 // PROJECT GET FETCH
@@ -89,11 +89,38 @@ function getProject() {
             // debugger
             const projectInfo = `
                 <div data-id=${project.id}>
-                <h3><li>${project.attributes.name}</li></h3>
-                <h5>$ ${project.attributes.budget} budget</h5>
+                <h3>${project.attributes.name}</h3>
+                <h4><li>Budget: $ ${project.attributes.budget}</li></h4>
                 </div>`;
 
-            document.querySelector('.modalInfo').innerHTML += projectInfo
+// <h4>HVAC</h4>
+//           <h6>Budget: $ 8000</h6>
+//           <ul>
+//             <li>Company 1 - Quote: $5000</li>
+//             <li>Company 2 - Quote: $6500</li>
+//             <li>Company 3 - Quote: $4000</li>
+//           </ul>
+
+            document.querySelector('.accordionItemContent').innerHTML += projectInfo
         })
     })
 }
+
+
+
+// ACCORDIAN
+
+var accItem = document.getElementsByClassName('accordionItem');
+    var accHD = document.getElementsByClassName('accordionItemHeading');
+    for (i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+    }
+    function toggleItem() {
+        var itemClass = this.parentNode.className;
+        for (i = 0; i < accItem.length; i++) {
+            accItem[i].className = 'accordionItem close';
+        }
+        if (itemClass == 'accordionItem close') {
+            this.parentNode.className = 'accordionItem open';
+        }
+    }
